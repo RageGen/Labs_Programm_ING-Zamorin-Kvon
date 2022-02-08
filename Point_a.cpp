0f42795fb35839f5f18f4fbb10bd9010875b18c9
RageGen <zamorin.daniil@mail.ru>
2022-02-08 17:33:43 +0300

    Point a and mirror errors

--- Point_a.cpp
+++ Point_a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 const int MATRIX_SIZE = 17;
 void MatrixCreation(int matrix[MATRIX_SIZE][MATRIX_SIZE])
@@ -23,11 +24,33 @@ void MatrixOut(int matrix[MATRIX_SIZE][MATRIX_SIZE])
         cout << endl;
     }
 }
+void SideDiagonal(int matrix[MATRIX_SIZE][MATRIX_SIZE],int SumOfNumbers)
+{
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            if (i + j == MATRIX_SIZE - 1)
+            {
+                SumOfNumbers = 0;
+                SumOfNumbers += (abs(matrix[i][j] / 10) + abs(matrix[i][j] % 10));
+            }
+        }
+        for (int j=0;j<MATRIX_SIZE;j++)
+        {
+            matrix[i][j]+=SumOfNumbers;
+        }
+    }
+}
 int main()
 {
     srand(time(nullptr));
     int matrix[MATRIX_SIZE][MATRIX_SIZE];
+    int SumOfNumbers=0;
     MatrixCreation(matrix);
+    MatrixOut(matrix);
+    SideDiagonal(matrix,SumOfNumbers);
+    cout<<"Point a:"<<endl;
     MatrixOut(matrix);
     return 0;
 }
