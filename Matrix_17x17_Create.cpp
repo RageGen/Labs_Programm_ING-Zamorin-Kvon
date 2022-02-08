d130465879cf7702b02f06b6f37ebb807dbb0ebd
RageGen <zamorin.daniil@mail.ru>
2022-02-08 16:31:23 +0300

    Matrix_17x17

---
+++ Matrix_17x17_Create.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+const int MATRIX_SIZE = 17;
+void MatrixCreation(int matrix[MATRIX_SIZE][MATRIX_SIZE])
+{
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            matrix[i][j] = rand() % 25 - 12;
+        }
+    }
+}
+void MatrixOut(int matrix[MATRIX_SIZE][MATRIX_SIZE])
+{
+    for (int i = 0; i < MATRIX_SIZE; i++)
+    {
+        for (int j = 0; j < MATRIX_SIZE; j++)
+        {
+            cout << setw(3) << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int matrix[MATRIX_SIZE][MATRIX_SIZE];
+    MatrixCreation(matrix);
+    MatrixOut(matrix);
+}
